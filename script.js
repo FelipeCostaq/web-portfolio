c28ef7bf7426183df9d6b077128b22d4558eacba
Felipe Costa <felipecostasiqu@gmail.com>
2024-12-31 22:05:20 -0300

    navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const links = document.querySelector('.links');
+
+mobileMenu.addEventListener('click', () => {
+    links.classList.toggle('active');
+    mobileMenu.classList.toggle('open');  
+});
